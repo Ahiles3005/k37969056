c544b1ff0225f0bf1774b65f6715e897748d86d3
Ruslan <ahiles3005@gmail.com>
2023-08-24 20:52:13 +0300

    ff

---
+++ hous/bitrix/php_interface/init.php
@@ -0,0 +1,246 @@
+<?
+
+// ROISTAT CODE BEGIN
+$roistatFilePath = __DIR__ . '/../../roistat/Roistat.php';
+if( file_exists($roistatFilePath) ) {
+    require_once $roistatFilePath;
+    Roistat::init();
+}
+// ROISTAT CODE END
+
+function _getHighloadTableValues($arProperty)
+{
+    CModule::IncludeModule('highloadblock');
+
+    $arResult = array();
+    static $hlblockCache = array();
+    static $directoryMap = array();
+    static $hlblockClassNameCache = array();
+    $tableName = $arProperty['USER_TYPE_SETTINGS']['TABLE_NAME'];
+
+    if (!empty($tableName)) {
+        if (!isset($hlblockCache[$tableName])) {
+            $hlblockCache[$tableName] = \Bitrix\Highloadblock\HighloadBlockTable::getList(
+                array(
+                    'select' => array('TABLE_NAME', 'NAME', 'ID'),
+                    'filter' => array('=TABLE_NAME' => $tableName)
+                )
+            )->fetch();
+        }
+        if (!empty($hlblockCache[$tableName])) {
+            if (!isset($directoryMap[$tableName])) {
+                $entity = \Bitrix\Highloadblock\HighloadBlockTable::compileEntity($hlblockCache[$tableName]);
+                $hlblockClassNameCache[$tableName] = $entity->getDataClass();
+                $directoryMap[$tableName] = $entity->getFields();
+                unset($entity);
+            }
+            if (!isset($directoryMap[$tableName]['UF_XML_ID']))
+                return $arResult;
+            $entityDataClass = $hlblockClassNameCache[$tableName];
+
+            $nameExist = isset($directoryMap[$tableName]['UF_NAME']);
+            $fileExists = isset($directoryMap[$tableName]['UF_FILE']);
+            $sortExist = isset($directoryMap[$tableName]['UF_SORT']);
+
+            $listDescr['order'] = array();
+            if ($sortExist) {
+                $listDescr['order']['UF_SORT'] = 'ASC';
+            }
+            if ($nameExist)
+                $listDescr['order']['UF_NAME'] = 'ASC';
+            else
+                $listDescr['order']['UF_XML_ID'] = 'ASC';
+            $listDescr['order']['ID'] = 'ASC';
+            /** @var \Bitrix\Main\DB\Result $rsData */
+            $rsData = $entityDataClass::getList($listDescr);
+            while ($arData = $rsData->fetch()) {
+                if (!$nameExist)
+                    $arData['UF_NAME'] = $arData['UF_XML_ID'];
+                $arData['SORT'] = ($sortExist ? $arData['UF_SORT'] : $arData['ID']);
+                $arResult[] = $arData;
+            }
+            unset($arData, $rsData);
+        }
+    }
+
+    return $arResult;
+}
+
+function GetHighloadExtendValue($arProperty, $value)
+{
+    static $arItemCache = array();
+
+    if (!isset($value['VALUE']) && !isset($value['ID']))
+        return false;
+    if (empty($arProperty['USER_TYPE_SETTINGS']['TABLE_NAME']))
+        return false;
+
+    $tableName = $arProperty['USER_TYPE_SETTINGS']['TABLE_NAME'];
+    if (!isset($arItemCache[$tableName]))
+        $arItemCache[$tableName] = array();
+
+    if (!empty($value['ID']) && isset($arItemCache[$tableName]) && is_array($arItemCache[$tableName])) {
+        foreach ($arItemCache[$tableName] as $arItem) {
+            if ($arItem['ID'] == $value['ID']) {
+                $value['VALUE'] = $arItem['UF_XML_ID'];
+                break;
+            }
+        }
+    }
+
+    if (!isset($arItemCache[$tableName][$value['VALUE']])) {
+        $arData = _getHighloadTableValues($arProperty);
+
+        if (!empty($arData)) {
+            if (is_array($arData)) {
+                foreach ($arData as $arItem) {
+                    $arItemCache[$tableName][$arItem['UF_XML_ID']] = $arItem;
+                    if (isset($arItem['UF_XML_ID']) && ($arItem['UF_XML_ID'] == $value['VALUE'] || $arItem['ID'] = $value['ID'])) {
+                        $arItem['VALUE'] = $arItem['UF_NAME'];
+                        if (isset($arItem['UF_FILE']))
+                            $arItem['FILE_ID'] = $arItem['UF_FILE'];
+                    }
+                }
+            }
+        }
+    }
+
+    if (isset($arItemCache[$tableName][$value['VALUE']])) {
+        return $arItemCache[$tableName][$value['VALUE']];
+    }
+    return false;
+}
+
+
+\Bitrix\Main\EventManager::getInstance()->addEventHandler('main', 'OnEpilog', 'SetLandingMeta');
+function SetLandingMeta()
+{
+    global $LANDING, $APPLICATION;
+    if (isset($LANDING['PROPERTIES'])) {
+        if (!empty($LANDING['PROPERTIES']['META_TITLE']['VALUE']['TEXT'])) {
+            $APPLICATION->SetPageProperty('title', $LANDING['PROPERTIES']['META_TITLE']['VALUE']['TEXT']);
+        }
+        if (!empty($LANDING['PROPERTIES']['META_KEYWORDS']['VALUE']['TEXT'])) {
+            $APPLICATION->SetPageProperty('keywords', $LANDING['PROPERTIES']['META_KEYWORDS']['VALUE']['TEXT']);
+        }
+        if (!empty($LANDING['PROPERTIES']['META_DESCRIPTION']['VALUE']['TEXT'])) {
+            $APPLICATION->SetPageProperty('description', $LANDING['PROPERTIES']['META_DESCRIPTION']['VALUE']['TEXT']);
+        }
+    }
+    //$APPLICATION->SetPageProperty('robots', "noindex, nofollow");
+}
+
+\Bitrix\Main\EventManager::getInstance()->addEventHandler('iblock', 'OnBeforeIBlockElementUpdate', 'CheckLandingUrl');
+\Bitrix\Main\EventManager::getInstance()->addEventHandler('iblock', 'OnBeforeIBlockElementAdd', 'CheckLandingUrl');
+function CheckLandingUrl(&$arFields)
+{
+    if ($arFields['IBLOCK_ID'] == 7) {
+        if (substr($arFields['CODE'], -1, 1) != '/') {
+            $arFields['CODE'] .= '/';
+        }
+    }
+    return true;
+}
+
+\Bitrix\Main\EventManager::getInstance()->addEventHandler('main', 'OnGetFileSRC', 'OnGetFileSRCHandler');
+function OnGetFileSRCHandler($arFields)
+{
+//    $contentType = explode('/', $arFields['CONTENT_TYPE']);
+//    file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/log_dev_img.txt', __FILE__ . ':' . __LINE__ . PHP_EOL . print_r($arFields, true) . "\n", FILE_APPEND);
+//    $fileSrc = '/upload/' . $arFields['SUBDIR'] . '/' . $arFields['FILE_NAME'];
+//    file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/log_dev_img.txt', __FILE__ . ':' . __LINE__ . PHP_EOL . print_r($fileSrc, true) . "\n", FILE_APPEND);
+//    if (CModule::IncludeModule("corvax.imgworker") && $contentType[0] == 'image') {
+//        return CCorvaxImgWorker::createImg($fileSrc);
+//    }
+    return false;
+}
+
+\Bitrix\Main\EventManager::getInstance()->addEventHandler('main', 'OnBeforeEventAdd', 'OnBeforeEventAdd');
+function OnBeforeEventAdd($event, $lid, &$arFields, $message_id, $files)
+{
+    if(isset($arFields['PRODUCT_PRICE']) && !$arFields['PRODUCT_PRICE']) {
+        $arFields['PRODUCT_PRICE'] = 'по запросу';
+    }
+}
+
+
+\Bitrix\Main\EventManager::getInstance()->addEventHandler('iblock', 'OnAfterIBlockElementAdd', 'ProjectRequest');
+\Bitrix\Main\EventManager::getInstance()->addEventHandler('iblock', 'OnAfterIBlockElementUpdate', 'ProjectRequest');
+function ProjectRequest($arFields)
+{
+    if($arFields['ID'] > 0  && $arFields['IBLOCK_ID'] == 14) {
+        $obElement = CIBlockElement::GetByID($arFields['ID'])->GetNextElement();
+
+        $arItem = $obElement->GetFields();
+        $arProperties = $obElement->GetProperties();
+
+        foreach ($arProperties as &$arProp) {
+            if($arProp['PROPERTY_TYPE'] == 'E' && $arProp['VALUE']) {
+                $arValue = array();
+                $arSort = array('sort' => 'asc');
+                $arFilter = array('ID' => $arProp['VALUE']);
+                $arSelect = array('ID', 'NAME');
+                $arGroup = false;
+                $arNavStartParams = false;
+                $dbResult = CIBlockElement::GetList($arSort, $arFilter, $arGroup, $arNavStartParams, $arSelect);
+                while($dbRes = $dbResult->GetNext())
+                {
+                    $arValue[$dbRes['ID']] = $dbRes['NAME'];
+                }
+                $arProp['DISPLAY_VALUE'] = $arValue;
+            } else {
+                $arProp = CIBlockFormatProperties::GetDisplayValue($obElement, $arProp, 'prsend');
+            }
+        }
+        unset($arProp);
+
+        $arSendFields = array(
+            'COMMENT' => $arItem['PREVIEW_TEXT'],
+            'DATE' => $arItem['DATE_CREATE'],
+        );
+        foreach ($arProperties as $arProp) {
+            if(is_array($arProp['DISPLAY_VALUE']))
+                $arProp['DISPLAY_VALUE'] = implode(', ', $arProp['DISPLAY_VALUE']);
+            $arSendFields[$arProp['CODE']] = $arProp['DISPLAY_VALUE'];
+        }
+
+        CEvent::Send('NEW_APPLICATION', 's1', $arSendFields, 'N');
+    }
+}
+function getReCaphaCode()
+{
+    return "6Le16pEUAAAAALSnV1OEBQiMih_Rx227yIv9BJZW";
+}
+
+function getReCaphaSecret()
+{
+    return "6Le16pEUAAAAAMLGhUQQ1SIMz1H3rBjAoMO4T7l5";
+}
+
+function CheckCapha($response)
+{
+    $status = false;
+
+    $url = 'https://www.google.com/recaptcha/api/siteverify';
+    $data = [
+        'secret' => getReCaphaSecret(),
+        'response' => $response
+    ];
+    $options = [
+        'http' => [
+            'method' => 'POST',
+            'content' => http_build_query($data)
+        ]
+    ];
+    $context = stream_context_create($options);
+    $verify = file_get_contents($url, false, $context);
+    $captcha_success = json_decode($verify);
+    if ($captcha_success->success == false) {
+        $status = false;
+    } else if ($captcha_success->success == true) {
+        // сохраняем данные, отправляем письма, делаем другую работу. Пользователь не робот
+        $status = true;
+    }
+    return $status;
+}
+?>
---
+++ teremik/bitrix/php_interface/init.php
@@ -0,0 +1,185 @@
+<?
+
+// ROISTAT CODE BEGIN
+$roistatFilePath = __DIR__ . '/../../roistat/Roistat.php';
+if( file_exists($roistatFilePath) ) {
+    require_once $roistatFilePath;
+    Roistat::init();
+}
+// ROISTAT CODE END
+
+function pre($item, $show_for = false)
+{
+    global $USER;
+    if ($USER->IsAdmin() || $show_for == 'all') {
+        if (!$item) echo ' <br />пусто <br />';
+        elseif (is_array($item) && empty($item)) echo '<br />массив пуст <br />';
+        else echo ' <pre>' . print_r($item, true) . ' </pre>';
+    }
+}
+
+function cropStr($str, $size)
+{
+    if (mb_strlen($str, 'utf-8') > $size)
+        $return_str = mb_substr($str, 0, mb_strpos($str, ' ', $size, 'utf-8'), 'utf-8') . " ...";
+    else $return_str = $str;
+    return $return_str;
+}
+
+function GetProperty($id)
+{
+    global $APPLICATION;
+    $val = $APPLICATION->GetPageProperty($id);
+    if ($val != "Y"): return false;
+    else: return true; endif;
+}
+
+function ShowProperty($id)
+{
+    global $APPLICATION;
+    $APPLICATION->AddBufferContent('GetProperty', $id);
+}
+
+/*
+AddEventHandler('main', 'OnEndBufferContent', 'controller404', 1001);
+
+function controller404(&$content) {
+   if(defined('ERROR_404') && ERROR_404 == 'Y') {
+        CHTTP::SetStatus('404 Not Found');
+      $content = file_get_contents($_SERVER["DOCUMENT_ROOT"].'/bitrix/templates/.default/404');
+      return false;
+   }
+}  
+*/
+
+\Bitrix\Main\EventManager::getInstance()->addEventHandler('iblock', 'OnAfterIBlockElementAdd', 'ProjectRequest');
+\Bitrix\Main\EventManager::getInstance()->addEventHandler('iblock', 'OnAfterIBlockElementUpdate', 'ProjectRequest');
+function ProjectRequest($arFields)
+{
+    if ($arFields['ID'] > 0 && $arFields['IBLOCK_ID'] == 26) {
+        $obElement = CIBlockElement::GetByID($arFields['ID'])->GetNextElement();
+
+        $arItem = $obElement->GetFields();
+        $arProperties = $obElement->GetProperties();
+
+        foreach ($arProperties as &$arProp) {
+            if ($arProp['PROPERTY_TYPE'] == 'E' && $arProp['VALUE']) {
+                $arValue = array();
+                $arSort = array('sort' => 'asc');
+                $arFilter = array('ID' => $arProp['VALUE']);
+                $arSelect = array('ID', 'NAME');
+                $arGroup = false;
+                $arNavStartParams = false;
+                $dbResult = CIBlockElement::GetList($arSort, $arFilter, $arGroup, $arNavStartParams, $arSelect);
+                while ($dbRes = $dbResult->GetNext()) {
+                    $arValue[$dbRes['ID']] = $dbRes['NAME'];
+                }
+                $arProp['DISPLAY_VALUE'] = $arValue;
+            } else {
+                $arProp = CIBlockFormatProperties::GetDisplayValue($obElement, $arProp, 'prsend');
+            }
+        }
+        unset($arProp);
+
+        $arSendFields = array(
+            'COMMENT' => $arItem['PREVIEW_TEXT'],
+            'DATE' => $arItem['DATE_CREATE'],
+        );
+        foreach ($arProperties as $arProp) {
+            if (is_array($arProp['DISPLAY_VALUE']))
+                $arProp['DISPLAY_VALUE'] = implode(', ', $arProp['DISPLAY_VALUE']);
+            $arSendFields[$arProp['CODE']] = $arProp['DISPLAY_VALUE'];
+        }
+
+        CEvent::Send('NEW_APPLICATION', 's1', $arSendFields, 'N');
+    }
+
+    if ($arFields['ID'] > 0 && $arFields['IBLOCK_ID'] == 5) {
+        \Bitrix\Main\Loader::includeModule('iblock');
+        \Bitrix\Main\Loader::includeModule('catalog');
+
+
+        $arPrice = CPrice::GetBasePrice($arFields['ID']);
+        CIBlockElement::SetPropertyValuesEx($arFields['ID'], false, array("MIN_PRICE" => $arPrice["PRICE"]));
+
+
+        $iblock_info = CCatalogSKU::GetInfoByProductIBlock($arFields['IBLOCK_ID']);
+
+        $success = array();
+
+        if (is_array($iblock_info) && !empty($iblock_info["SKU_PROPERTY_ID"])) {
+            $rsOffers = CIBlockElement::GetList(array("PRICE" => "ASC",), array("IBLOCK_ID" => $iblock_info["IBLOCK_ID"], "ACTIVE" => "Y"), array("ID", "PROPERTY_" . $iblock_info["SKU_PROPERTY_ID"]));
+            while ($arOffer = $rsOffers->GetNext()) {
+
+
+                $offer_price = GetCatalogProductPrice($arOffer["ID"], 1); // 1 - это ID типа цены
+                $product_id = $arOffer["PROPERTY_" . $iblock_info["SKU_PROPERTY_ID"] . "_VALUE"];
+
+                // проверка
+                if (array_key_exists($product_id, $success) && $success[$product_id] < $offer_price["PRICE"])
+                    continue;
+
+                CIBlockElement::SetPropertyValuesEx($product_id, false, array("MIN_PRICE" => $offer_price["PRICE"]));
+                $success[$product_id] = $offer_price["PRICE"];
+            }
+        }
+
+    }
+
+}
+
+if (!function_exists('getHBlock')) {
+    function getHBlock($id = false)
+    {
+
+        CModule::IncludeModule("highloadblock");
+        if (!$id) return false;
+        $hlbl = $id;
+        $hlblock = \Bitrix\Highloadblock\HighloadBlockTable::getById($hlbl)->fetch();
+        $entity = \Bitrix\Highloadblock\HighloadBlockTable::compileEntity($hlblock);
+        $entity_data_class = $entity->getDataClass();
+        return $entity_data_class;
+    }
+}
+if (!function_exists('getMessagerPhone')) {
+    function getMessagerPhone($phone)
+    {
+        $hlb_class = getHBlock(6);
+        $arResult = $hlb_class::getList(["filter" => ["UF_PHONE" => $phone]])->fetch();
+        return $arResult;
+    }
+}
+
+
+if (!function_exists('getMessagerPhones')) {
+    function getMessagerPhones()
+    {
+        $hlb_class = getHBlock(6);
+        $arResult = $hlb_class::getList()->fetchAll();
+        return $arResult;
+    }
+}
+
+if (!function_exists('getHTMLPhoneByType')) {
+    function getHTMLPhoneByType($types, $phones)
+    {
+        $arResult = "";
+        foreach ($types as $type) {
+            switch ($type) {
+                case 11: #telegram
+                    $arResult .= "<a class='telegram' title='Написать в Telegram' target='_blank' href='https://telegram.me/" . $phones[$type] . "'></a>";
+                    break;
+                case 12:#viber
+                    $arResult .= "<a class='viber' title='Написать в Viber' target='_blank' href='viber://chat?number=%2B" . $phones[$type] . "'></a>";
+                    break;
+                case 13:#whatsapp
+                    $arResult .= "<a class='whatsapp' title='Написать в Whatsapp' target='_blank' href='https://wa.me/" . $phones[$type] . "'></a>";
+                    break;
+            }
+        }
+        return $arResult;
+    }
+}
+
+
+?>
